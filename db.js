4417af32c280c8d135bef7172dc04610db564a9f
Ata00422 <167747030+Ata00422@users.noreply.github.com>
2026-02-25 19:22:00 +0500

    25 Feb Push

---
+++ db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const mongoUrl = process.env.Mongodb_Url;
+
+const connectToMongoDb = async () => {
+    try {
+        await mongoose.connect(mongoUrl);
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log("Error: ", error.message);
+        process.exit(1);
+    }
+}
+
+export default connectToMongoDb;
---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors"
+import connectToMongoDb from "./db.js";
+import productController from "./Controllers/ProductController.js";
+import GameCatController from "./Controllers/GameCatController.js";
+import CategoryController from "./Controllers/CategoryController.js";
+import UserController from "./Controllers/UserController.js";
+import trackController from "./Controllers/TrackRecordController.js";
+import favController from "./Controllers/FavouriteController.js";
+import blogsController from "./Controllers/BlogsController.js";
+const app = express()
+app.use(cors({
+    origin: "*"
+}))
+app.use(express.json())
+connectToMongoDb()
+
+app.use("/api/gameCat", GameCatController)
+app.use("/api/category", CategoryController)
+app.use("/api/games", productController)
+app.use("/api/user", UserController)
+app.use("/api/track", trackController)
+app.use("/api/fav", favController)
+app.use("/api/blogs", blogsController)
+
+app.listen(8000, () => {
+    console.log("App listing at http://localhost:8000");
+})
